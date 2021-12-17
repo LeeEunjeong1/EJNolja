0e685df20be596f43704f845a5e68cd8748b9c92
Eunjeong <tbig1019@gmail.com>
2021-12-17 18:08:32 +0900

    Main Fragment

---
+++ app/src/main/java/com/example/ejnolja/view/MainRestFragment.kt
@@ -0,0 +1,33 @@
+package com.example.ejnolja.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ejnolja.BlankViewModel
+import com.example.ejnolja.R
+
+class MainRestFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainReservationFragment()
+    }
+
+    private lateinit var viewModel: BlankViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_rest, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(BlankViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
