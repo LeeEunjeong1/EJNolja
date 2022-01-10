bd9734b6ae5fd7203ae8de062de308b247f80465
Eunjeong <tbig1019@gmail.com>
2022-01-10 16:49:19 +0900

    메인 수정

--- app/src/main/java/com/example/ejnolja/view/MainActivity.kt
+++ app/src/main/java/com/example/ejnolja/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ejnolja.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejnolja.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,29 +12,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
-        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+        var bnv_main = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
-        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
-        bnv_main.run { setOnNavigationItemSelectedListener {
+        bnv_main.run { setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.first -> {
+                R.id.tab1 -> {
                     // 다른 프래그먼트 화면으로 이동하는 기능
-                    val homeFragment = HomeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+                    val restFragment = MainRestFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, restFragment).commit()
                 }
-                R.id.second -> {
-                    val boardFragment = BoardFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, boardFragment).commit()
+                R.id.tab2 -> {
+                    val searchFragment = MainSearchFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, searchFragment).commit()
                 }
-                R.id.third -> {
-                    val settingFragment = SettingFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, settingFragment).commit()
+                R.id.tab3 -> {
+                    val reservationFragment = MainReservationFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_layout, reservationFragment).commit()
                 }
             }
             true
         }
-            selectedItemId = R.id.first
+            selectedItemId = R.id.tab1
         }
     }
 }
